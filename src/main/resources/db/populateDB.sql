04a1ef3f4a26637f0b6650cfb383d6c70d95137d
DC Denton <denton.87@mail.ru>
2022-10-25 21:38:35 +0400

    HW03 fix spring + db meals create

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,13 +1,23 @@
-DELETE
-FROM user_roles;
-DELETE
-FROM users;
+DELETE FROM user_roles;
+DELETE FROM meals;
+DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
 VALUES ('User', 'user@yandex.ru', 'password'),
        ('Admin', 'admin@gmail.com', 'admin'),
        ('Guest', 'guest@gmail.com', 'guest');
+
+INSERT INTO meals (datetime, description, calories, user_id)
+VALUES
+('2020-01-30 10:00', 'Завтрак', 500, 100000),
+('2020-01-30 13:00', 'Обед', 1000, 100000),
+('2020-01-30 20:00', 'Ужин', 500, 100000),
+('2020-01-31 0:00', 'Еда на граничное значение', 100, 100000),
+('2020-01-31 10:00', 'Завтрак', 1000, 100000),
+('2020-01-31 13:00', 'Обед', 500, 100000),
+('2020-01-31 20:00', 'Ужин', 410, 100000),
+('2020-01-31 10:00', 'Еда админа', 500, 100001);
 
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
